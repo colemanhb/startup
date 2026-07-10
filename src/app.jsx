f9b4bf4d75dd014456cd3d5e137f38553879bf46
colemanhb <colemanhallbrown@gmail.com>
2026-07-09 23:28:58 -0600

    implementing NavLink

--- src/app.jsx
+++ src/app.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Languages } from './languages/languages';
+import { Page } from './page/page';
+import { NewWords } from './new_words/new_words';
+import { About } from './about/about';
+import { Booklist } from './booklist/booklist';
+
 export default function App() {
-  return <div className="body bg-light text-dark">
+  return (
+    <BrowserRouter>
+        <div className="body bg-light text-dark">
             <header>
                 <div className="main-buttons">
                     <button id="menu-button" className="btn btn-light">☰</button>
@@ -14,11 +24,11 @@ export default function App() {
                 <nav id="sidebar" className="open">
                     <h2>LibreBoox</h2>
                     <menu>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="languages.html">Languages</a></li>
-                    <li><a href="page.html">Continue Reading</a></li>
-                    <li><a href="new_words.html">New Words</a></li>
-                    <li><a href="about.html">About</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/languages">Languages</NavLink></li>
+                    <li><NavLink to="/page">Continue Reading</NavLink></li>
+                    <li><NavLink to="/new-words">New Words</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
                     </menu>
                 </nav>
                 <aside id="settings-panel" className="open">
@@ -34,9 +44,15 @@ export default function App() {
                 <hr />
             </header>
 
-            <main>
-                App components go here.
-            </main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/languages' element={<Languages />} />
+                <Route path='/page' element={<Page />} />
+                <Route path='/new-words' element={<NewWords />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/booklist' element={<Booklist />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
             <footer>
                 <span className="text-reset">Coleman Hall-Brown</span>
@@ -46,5 +62,11 @@ export default function App() {
                 <div id="alquimista" className="picture-box"><img height="100px" src="alquimista.jpg" alt="The Alchemist" /></div>
                 <a href="https://github.com/colemanhb/startup/tree/main/libreboox-html">GitHub</a>
             </footer>
-        </div>;
+        </div>
+    </BrowserRouter>
+    );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
