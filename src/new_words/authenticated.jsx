a7fc9ad2588115001a5e8312f969729512a6204b
colemanhb <colemanhallbrown@gmail.com>
2026-07-15 21:13:12 -0600

    functionality for switching between your words and friends words

--- src/new_words/authenticated.jsx
+++ src/new_words/authenticated.jsx
@@ -1,25 +1,66 @@
 import './new_words.css';
 import React from 'react';
 
+const my_dict = {
+  libro: {
+    definition: "A set of handwritten or printed sheets of paper that, when sewn or bound together, form a volume.",
+    translatedDefinition: "Conjunto de hojas de papel manuscritas o impresas que, cosidas o encuadernadas, forman un volumen.",
+    translation: "Book"
+  },
+  lugar: {
+    definition: "A particular position or point in space.",
+    translatedDefinition: "Una posición o punto particular en el espacio.",
+    translation: "Place"
+  },
+}
+
+const other_dict = {
+  maison: {
+    definition: "A building for human habitation, especially one that is lived in by a family or small group of people.",
+    translatedDefinition: "Un edificio para la habitación humana, especialmente uno en el que vive una familia o un pequeño grupo de personas.",
+    translation: "House"
+  },
+  ville: {
+    definition: "A large town.",
+    translatedDefinition: "Una ciudad grande.",
+    translation: "City"
+  },
+}
+
 export function Authenticated() {
+  const [wordSet, setWordSet] = React.useState('myWords');
   return (
     <main className="new-words">
       <div className="new-words-controls">
-        <button id="my-words" className="btn btn-light">My Words</button>
-        <button id="friends-words" className="btn btn-light">My Friends' Words</button>
+        <button id="my-words" className="btn btn-light" onClick={() => setWordSet('myWords')}>
+          My Words
+        </button>
+        <button id="friends-words" className="btn btn-light" onClick={() => setWordSet('friendsWords')}>
+          My Friends' Words
+        </button>
       </div>
       <table className="table table-bordered">
         <thead>
             <tr>
                 <th>Word</th>
+                <th>Translation</th>
                 <th>Definition</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Libro</td>
-                <td>Conjunto de hojas de papel manuscritas o impresas que, cosidas o encuadernadas, forman un volumen. This is not the definition of the word, but I am adding more text to show how the css handles very long definitions.</td>
+          {wordSet === 'myWords' ? Object.entries(my_dict).map(([word, data]) => (
+            <tr key={word}>
+              <td>{word}</td>
+              <td>{data.translation}</td>
+              <td>{data.definition}</td>
             </tr>
+          )) : Object.entries(other_dict).map(([word, data]) => (
+            <tr key={word}>
+              <td>{word}</td>
+              <td>{data.translation}</td>
+              <td>{data.definition}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
