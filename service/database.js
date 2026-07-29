2b739abb1347dbe2261239500aef0ef27fff5901
colemanhb <colemanhallbrown@gmail.com>
2026-07-29 12:14:35 -0600

    added functions to database.js

--- service/database.js
+++ service/database.js
@@ -38,18 +38,69 @@ async function updateUserRemoveAuth(user) {
   await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
 }
 
-async function addScore(score) {
-  return scoreCollection.insertOne(score);
+async function saveWord(username, word, definition) {
+  return wordCollection.updateOne(
+    { username, word },
+    { $set: { username, word, definition } },
+    { upsert: true }
+  );
 }
 
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
+async function getUserWords(username) {
+  const words = await wordCollection.find({ username }).toArray();
+  const wordMap = {};
+  words.forEach(item => { wordMap[item.word] = item.definition; });
+  return wordMap;
+}
+
+async function getAllWordsExcept(username) {
+  const words = await wordCollection.find({ username: { $ne: username } }).toArray();
+  const wordMap = {};
+  words.forEach(item => { 
+    if (!wordMap[item.username]) wordMap[item.username] = {};
+    wordMap[item.username][item.word] = item.definition; });
+  return wordMap;
+}
+
+async function saveBookProgress(username, bookId, page) {
+  const stringId = String(bookId);
+  await progressCollection.updateOne(
+    { username, bookId: stringId },
+    { $set: { username, bookId: stringId, page } },
+    { upsert: true }
+  );
+}
+
+async function getBookProgress(username, bookId) {
+  const record = await progressCollection.findOne({ username, bookId: String(bookId) });
+  return record ? record.page : 0;
+}
+
+async function saveLastBook(username, bookId) {
+  await userCollection.updateOne(
+    { username },
+    { $set: { lastBookId: String(bookId) } },
+  );
+}
+
+async function getLastBook(username) {
+  const user = await userCollection.findOne({ username });
+  return user?.lastBookId || null;
+}
+
+async function saveSettings(username, settings) {
+  await settingCollection.updateOne(
+    { username },
+    { $set: { username, ...settings } },
+    { upsert: true }
+  );
+}
+
+async function getSettings(username) {
+  const settings = await settingCollection.findOne({ username });
+  if (!settings) return { theme: 'light', fontSize: 1.0 };
+  const { _id, username: u, ...cleanSettings } = settings;
+  return cleanSettings;
 }
 
 module.exports = {
@@ -58,6 +109,7 @@ module.exports = {
   addUser,
   updateUser,
   updateUserRemoveAuth,
-  addScore,
-  getHighScores,
+  saveWord,
+  getUserWords,
+  getAllWordsExcept,
 };
